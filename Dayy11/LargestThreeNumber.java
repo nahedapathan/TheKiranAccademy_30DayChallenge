69c2926b7afbde298c97e9b5f434d05248d5e2be
Pathan Naheda Mohammed khan <99787055+nahedapathan@users.noreply.github.com>
2024-09-12 20:47:57 +0530

    Add files via upload

---
+++ Dayy11/LargestThreeNumber.java
@@ -0,0 +1,22 @@
+package Dayy11;
+
+public class LargestThreeNumber {
+	public static void main(String[] args) {
+		int a=10;
+		int b=5;
+		int c=6;
+		if(a>b && a>c)
+		{
+			System.out.println(a+" is largest Number");
+		}
+		else if(b>a && b>c)
+		{
+			System.out.println(b+" is largest Number");
+		}
+		else
+		{
+			System.out.println(c+" is largest Number");
+		}
+	}
+
+}
---
+++ Dayy11/PosNegZeroNo.java
@@ -0,0 +1,21 @@
+package Dayy11;
+
+public class PosNegZeroNo {
+	public static void main(String[] args) {
+		int n=10;
+		if(n==0)
+		{
+			System.out.println(n+" Number is Zero");
+		}
+		else if(n>0)
+		{
+			System.out.println(n+" Number is Positive");
+		}
+		else
+		{
+			System.out.println(n+" Number is Negative");
+		}
+	}
+	
+
+}
